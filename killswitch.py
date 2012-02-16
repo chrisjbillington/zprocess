720c526bebb2b1ec48b7a605bd9481b55ca017b6
cbilling <cbilling@localhost>
2012-02-16 12:33:16 +0000

    Added suproc_utils module. With it, you can launch a subprocess with two queues connecting you to it, and with the subprocess tied to the parent so that it will be killed when the parent process ends. Or you can use that kill_on_exit(process) function by itself, on either a multiprocessing.Process or subprocess.Popen object. The functions are all separate enough that you could use them to have multiple subprocesses all communicate with a parent with the same pair of queues. These are really just a collection of convenience functions to consolidate some common operations I find myself doing with subprocesses and queues. All the functionality is from the standard library already.

---
+++ killswitch.py
@@ -0,0 +1,19 @@
+import sys, os, time, signal
+
+child_pid = int(sys.argv[1])
+parent_pid = int(sys.argv[2])
+
+while True:
+    try:
+        os.kill(child_pid ,0) # Doesn't actually kill, just checks if the process ID exists
+    except OSError:
+        # The child is dead. Exit.
+        break
+    try:
+        os.kill(parent_pid,0)
+    except OSError:
+        # The parent is dead. Kill the child, then exit.
+        os.kill(child_pid,signal.SIGKILL)
+        break
+    time.sleep(1)
+
