0749e3080d1d2215ce52e0149834ba1fd5be4d29
cbilling <cbilling@localhost>
2012-03-07 10:32:34 +0000

    Modified subproc_utils module to use zmq for IPC. Modified lyse to accomodate the slight change.

--- example_client.py
+++ example_client.py
@@ -5,6 +5,8 @@ to_parent, from_parent = setup_connection_with_parent()
 item = from_parent.get()
 print 'client: got an item: '+ item
 to_parent.put(item)
+to_parent.put(item)
+to_parent.put(item)
 
 import time
 # To prove that this process gets killed when its parent ends:
--- example_server.py
+++ example_server.py
@@ -1,5 +1,5 @@
 from subproc_utils import subprocess_with_queues
 
-to_child, from_child, child, manager = subprocess_with_queues('example_client.py')
+to_child, from_child, child = subprocess_with_queues('example_client.py')
 to_child.put('<Some item!>')
 print 'server: got the item back: '+ from_child.get()
