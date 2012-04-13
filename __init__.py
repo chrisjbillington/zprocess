a829b5c528ca60489db4f8610048a50169ba30cb
Chris Billington <chrisjbillington@gmail.com>
2012-04-13 03:33:39 +0000

    Added heartbeats! Child termination is now guaranteed when the parent dies by using heartbeats instead of a process-watcher. A thread in the child polls the parent once a second, and if it doesn't respond within a second, the child kills itself.

--- __init__.py
+++ __init__.py
@@ -3,6 +3,56 @@ import sys
 import subprocess
 import threading
 import zmq
+import time
+import signal
+
+class HeartbeatServer(object):
+    """A server which recieves messages from clients and echoes them
+    back. There is only one server for however many clients there are"""
+    def __init__(self):
+        self.running = False
+        
+    def run(self, context):
+        if not self.running:
+            self.running = True
+            self.sock = context.socket(zmq.REP)
+            self.port = self.sock.bind_to_random_port('tcp://127.0.0.1')
+            self.mainloop_thread = threading.Thread(target=self.mainloop)
+            self.mainloop_thread.daemon = True
+            self.mainloop_thread.start()
+        return self.port
+        
+    def mainloop(self):
+        while True:
+            msg = self.sock.recv()
+            self.sock.send(msg)
+
+class HeartbeatClient(object):
+    def __init__(self,context, server_port):
+        self.sock = context.socket(zmq.REQ)
+        self.poller = zmq.Poller()
+        self.poller.register(self.sock,zmq.POLLIN)
+        self.port = self.sock.connect('tcp://127.0.0.1:%s'%server_port)
+        self.mainloop_thread = threading.Thread(target=self.mainloop)
+        self.mainloop_thread.daemon = True
+        self.mainloop_thread.start()
+        self.pid = str(os.getpid())
+
+    def mainloop(self):
+        while True:
+            try:
+                time.sleep(1)
+                self.sock.send(self.pid, zmq.NOBLOCK)
+                events = self.poller.poll(1000)
+                if events:
+                    msg = self.sock.recv()
+                    assert msg == self.pid
+                else:
+                    break
+            except Exception as e:
+                break
+        os.kill(os.getpid(), signal.SIGKILL)
+            
     
 class WriteQueue(object):
     """Provides writing of python objects to the underlying zmq socket,
@@ -52,7 +102,9 @@ def subprocess_with_queues(path):
     port_from_child = from_child.bind_to_random_port('tcp://127.0.0.1')
     to_self.connect('tcp://127.0.0.1:%s'%port_from_child)
     
-    child = subprocess.Popen([sys.executable, '-u', path, str(port_from_child)])
+    heartbeat_port = heartbeat_server.run(context)
+    
+    child = subprocess.Popen([sys.executable, '-u', path, str(port_from_child),str(heartbeat_port)])
     
     port_to_child = from_child.recv()
     to_child.connect('tcp://127.0.0.1:%s'%port_to_child)
@@ -60,12 +112,11 @@ def subprocess_with_queues(path):
     to_child = WriteQueue(to_child)
     from_child = ReadQueue(from_child, to_self)
     
-    kill_on_exit(child)
-    
     return to_child, from_child, child
     
 def setup_connection_with_parent():
     port_to_parent = int(sys.argv[1])
+    port_to_heartbeat_server = int(sys.argv[2])
 
     context = zmq.Context()
     
@@ -81,5 +132,9 @@ def setup_connection_with_parent():
     
     from_parent = ReadQueue(from_parent, to_self)
     to_parent = WriteQueue(to_parent)
-    return to_parent, from_parent
     
+    global heartbeat_client
+    heartbeat_cient = HeartbeatClient(context, port_to_heartbeat_server) 
+    return to_parent, from_parent
+
+heartbeat_server = HeartbeatServer()
--- example_server.py
+++ example_server.py
@@ -2,4 +2,4 @@ from subproc_utils import subprocess_with_queues
 
 to_child, from_child, child = subprocess_with_queues('example_client.py')
 to_child.put(['<Some item!>','<some data!>'])
-print 'server: got the item back: '+ from_child.get()
+print 'server: got the item back: ', from_child.get()
