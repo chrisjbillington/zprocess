61476008774d093ce354d0b6c1ccbf169b835514
cbilling <cbilling@localhost>
2012-09-27 11:15:10 +0000

    Made more robust against failure. Made it restart more cleanly. Fixed a bug where releasing a lock that was not held crashed instead of returning the error to the client.

--- __main__.py
+++ __main__.py
@@ -48,6 +48,28 @@ class ZMQLockServer(object):
         self.timeout_monitor.daemon = True
         self.timeout_monitor.start()
         
+        context = zmq.Context.instance()
+        # We have an extra ROUTER-DEALER layer before our REP socket
+        # so that we can monitor for incoming requests from clients
+        # on the ROUTER before having sent a response to the current
+        # client. Otherwise the REP socket hides this from us.
+
+        self.router = context.socket(zmq.ROUTER)
+        self.dealer = context.socket(zmq.DEALER)
+        self.sock = context.socket(zmq.REP)
+        
+        self.poller = zmq.Poller()
+        self.poller.register(self.router, zmq.POLLIN)
+        
+        # Bind the router to the outside world:            
+        self.router.bind('tcp://0.0.0.0:%d'%self.port)
+
+        # Bind the REP socket to an inproc handle:
+        self.sock.bind('inproc://to-rep-socket')
+        
+        # Connect the dealer to the rep socket:
+        self.dealer.connect('inproc://to-rep-socket')
+        
     def acquire(self, filepath, uuid, timeout):
         timeout = int(timeout)
         with self.access_lock:
@@ -80,43 +102,23 @@ class ZMQLockServer(object):
                 return False, None
         
     def run(self):
-        context = zmq.Context.instance()
-        # We have an extra ROUTER-DEALER layer before our REP socket
-        # so that we can monitor for incoming requests from clients
-        # on the ROUTER before having sent a response to the current
-        # client. Otherwise the REP socket hides this from us.
-
-        router = context.socket(zmq.ROUTER)
-        dealer = context.socket(zmq.DEALER)
-        sock = context.socket(zmq.REP)
-        
-        poller = zmq.Poller()
-        poller.register(router, zmq.POLLIN)
-        
-        # Bind the router to the outside world:            
-        router.bind('tcp://0.0.0.0:%d'%self.port)
-
-        # Bind the REP socket to an inproc handle:
-        sock.bind('inproc://to-rep-socket')
-        
-        # Connect the dealer to the rep socket:
-        dealer.connect('inproc://to-rep-socket')
         logger.info('This is zlock server, running on port %d'%self.port)
         while True:
             # Forward a (multipart) message from the router, through the dealer, to the REP socket:
-            message = router.recv_multipart()
-            dealer.send_multipart(message)
+            message = self.router.recv_multipart()
+            self.dealer.send_multipart(message)
             # Pull the same message out of the REP socket
-            messages = sock.recv_multipart()
+            messages = self.sock.recv_multipart()
             try:
                 request = messages[0]
                 if request == 'hello':
-                    sock.send('hello')
+                    self.sock.send('hello')
+                    logger.info('someone said hello')
                 elif request == 'acquire':
                     args = messages[1:]
                     success, data = self.acquire(*args)
                     if success:
-                        sock.send_multipart(['ok','lock acquired, re-entry depth %d'%data])
+                        self.sock.send_multipart(['ok','lock acquired, re-entry depth %d'%data])
                         logger.info('%s %sacquired %s'%(args[1], 're-entrantly ' if data > 1 else '', args[0]))
                     else:
                         # Wait until next event, or RETRY_INTERVAL if no
@@ -125,34 +127,32 @@ class ZMQLockServer(object):
                         # immediately.  This is much better than the client
                         # retrying every .1 seconds or something, not knowing
                         # whether there's been any activity on the server:
-                        events = poller.poll(self.retry_interval)
-                        sock.send_multipart(['retry', 'lock held by uuid %s'%data])
+                        events = self.poller.poll(self.retry_interval)
+                        self.sock.send_multipart(['retry', 'lock held by uuid %s'%data])
                         logger.info('%s failed to acquire %s, because %s is holding it'%(args[1], args[0], data))
                 elif request == 'release':
                     args = messages[1:]
                     success, data = self.release(*args)
                     if success:
-                        sock.send_multipart(['ok','lock released, re-entry depth %d'%data])
+                        self.sock.send_multipart(['ok','lock released, re-entry depth %d'%data])
                         if data:
                             logger.info('%s lowered its re-entrance level on %s'%(args[1], args[0]))
-                            pass
                         else:
                             logger.info('%s released %s'%(args[1], args[0]))
-                            pass
                     else:
-                        sock.send_multipart(['error','lock holding timed out or was never acquired'])
-                        logger.warning('%s tried to released %s, but it wasn\'t holding it at the time'%(args[2], args[1]))
+                        self.sock.send_multipart(['error','lock holding timed out or was never acquired'])
+                        logger.warning('%s tried to release %s, but wasn\'t holding it at the time'%(args[1], args[0]))
                 else:
                     raise ValueError('invalid method: %s'%request)
             except Exception:
                 traceback_lines = traceback.format_exception(sys.exc_type, sys.exc_value, sys.exc_traceback)
                 message = ''.join(traceback_lines)
-                sock.send_multipart(['error',message])
                 logger.error('Exception whilst processing request %s:\n%s'%(str(messages), message))
+                self.sock.send_multipart(['error',message])
             # And finally, forward the response from the dealer back
             # through the router to the client:
-            message = dealer.recv_multipart()
-            router.send_multipart(message)
+            message = self.dealer.recv_multipart()
+            self.router.send_multipart(message)
             
     def monitor_timeouts(self):
         while True:
@@ -174,19 +174,23 @@ class ZMQLockServer(object):
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     logger = setup_logging()
+    # First instantiation outside the while loop, so a failure to initialise will quit rather than loop forever:
+    server = ZMQLockServer(SERVER_PORT, RETRY_INTERVAL)
     while True:
         try:
-            server = ZMQLockServer(SERVER_PORT, RETRY_INTERVAL)
-            try:
-                server.run()
-            except KeyboardInterrupt:
-                logger.info('KeyboardInterrupt, stopping')
-                break
+            server.run()
+        except KeyboardInterrupt:
+            logger.info('KeyboardInterrupt, stopping')
+            break
         except Exception:
             traceback_lines = traceback.format_exception(sys.exc_type, sys.exc_value, sys.exc_traceback)
             message = ''.join(traceback_lines)
             logger.critical('unhandled exception, attempting to restart:\n%s'%message)
-            
+            # Close all sockets:
+            context = zmq.Context.instance()
+            context.destroy(linger=False)
+            # Re-initialise the server:
+            server = ZMQLockServer(SERVER_PORT, RETRY_INTERVAL)
             
             
             
