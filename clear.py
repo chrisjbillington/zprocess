84d2c2139d0f23a271c6b3a7169e787c405e9fb5
cbilling <cbilling@localhost>
2012-10-04 07:10:44 +0000

    removed accidentally print of None returned from function

--- clear.py
+++ clear.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     host, port = zlock.guess_server_address()   
     print 'Clearing %s locks on zlock server %s %s:'%('*all*' if clear_all else 'expired', host, port)
     zlock.connect(host, port)
-    print zlock.clear(clear_all)
+    zlock.clear(clear_all)
     print zlock.status()
