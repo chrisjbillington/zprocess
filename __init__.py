62a2322e4c4b8bd4bd4304e29bb73541447272d1
cbilling <cbilling@localhost>
2013-08-01 11:40:03 +0200

    Added methods
    set_client_process_name(name)
    
    and
    
    set_client_thread_name(name)
    
    for setting custom names for processes and threads so that zlocks log has more meaningful client names.

--- __init__.py
+++ __init__.py
@@ -13,11 +13,23 @@ MIN_CACHE_TIME = 0.1 # seconds
 MAX_CACHE_TIME = 1 # seconds
 DEFAULT_PORT = 7339
 
+process_identifier = str(os.getpid())
+thread_identifier_threadlocal = threading.local()
+
+def set_client_process_name(name):
+    global process_identifier
+    process_identifier = name + '-' + str(os.getpid())
+    
+def set_client_thread_name(name):
+    thread_identifier_threadlocal.identifier = name + '-' + threading.current_thread().name
+    
 def get_client_id():
+    try:
+        thread_identifier = thread_identifier_threadlocal.identifier
+    except AttributeError:
+        thread_identifier = thread_identifier_threadlocal.identifier = threading.current_thread().name
     host_name = socket.gethostname()
-    process_id = str(os.getpid())
-    thread_name= threading.current_thread().name
-    return ':'.join([host_name,process_id,thread_name])
+    return ':'.join([host_name, process_identifier,thread_identifier])
 
 def raise_exception_in_thread(exc_info):
     """Raises an exception in a thread"""
