8c5e8f6d08418e77ec713b2f1c5283d621cf8ec3
cbilling <cbilling@localhost>
2014-01-24 15:42:05 +1100

    Deleted unused submodules.

--- locking/status.py
+++
@@ -1,8 +0,0 @@
-import zlock
-import sys
-
-if __name__ == '__main__':
-    host, port = zlock.guess_server_address()   
-    print 'status of zlock server on %s %s is:'%(host, port)
-    zlock.connect(host, port)
-    print zlock.status()
