f111ee4c68381ee4919a0967ae9311395f0ab78a
pstarkey <pstarkey@localhost>
2016-01-09 15:41:05 +1100

    Updated examples for Python 3 compatibility (print function)

--- zprocess/examples/example_server.py
+++ zprocess/examples/example_server.py
@@ -11,6 +11,7 @@
 #                                                                   #
 #####################################################################
 
+from __future__ import print_function
 from zprocess import subprocess_with_queues, Event
 import time
 
@@ -18,12 +19,12 @@ to_child, from_child, child = subprocess_with_queues('example_client.py')
 
 # The normal kind of directly passing data to the child:
 to_child.put(['<Some item!>','<some data!>'])
-print 'server: got the item back: ', from_child.get()
+print('server: got the item back: ', from_child.get())
 
 # Posting an event that all processes in the tree can see (if there were more processes, of course):
 foo_event = Event('foo',type='post')
 
 for i in range(10):
     time.sleep(0.5)
-    print 'server: posting a foo event with id=%d'%i
+    print('server: posting a foo event with id=%d'%i)
     foo_event.post(i, data='hello!')
