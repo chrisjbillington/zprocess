cb46e6a8127fbcf6e611ff7f3328b488733bfe9d
pstarkey <philipstarkey@users.noreply.github.com>
2016-01-09 15:41:05 +1100

    Updated examples for Python 3 compatibility (print function)

--- zprocess/examples/processclass_example.py
+++ zprocess/examples/processclass_example.py
@@ -11,15 +11,16 @@
 #                                                                   #
 #####################################################################
 
+from __future__ import print_function
 from zprocess import Process
 import os
 
 class Foo(Process):
     def run(self, data):
-        print 'this is a running foo in process', os.getpid()
-        print 'data is', data
+        print('this is a running foo in process', os.getpid())
+        print('data is', data)
         message = self.from_parent.get()
-        print 'foo, got a message:', message
+        print('foo, got a message:', message)
         self.to_parent.put('hello yourself!')
 
 # This __main__ check is important to stop the same code executing again in the child:
@@ -28,4 +29,4 @@ if __name__ == '__main__':
     to_child, from_child = foo.start('bar')
     to_child.put('hello, foo!')
     response = from_child.get()
-    print 'parent, got a response:', response
+    print('parent, got a response:', response)
