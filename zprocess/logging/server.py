9953a7b873b527db2703211d4ca9752ef95c18cd
Chris Billington <chrisjbillington@gmail.com>
2018-09-20 23:27:22 -0400

    Zlog server and client in place.
    Needs testing and actual configuration and rollover of log files

--- zprocess/logging/server.py
+++ zprocess/logging/server.py
@@ -18,6 +18,7 @@ FILE_CLOSE_TIMEOUT = 15
 
 ERR_INVALID_COMMAND = b'error: invalid command'
 ERR_WRONG_NUM_ARGS = b'error: wrong number of arguments'
+ERR_BAD_ENCODING = b'error: filepath not UTF8 encoded'
 
 PROTOCOL_VERSION = '1.0.0'
 
@@ -47,20 +48,112 @@ def setup_logging():
         logging.warning(msg)
 
 
+class LogFile(object):
+    instances = {}
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.file = None
+        self.clients = set()
+
+    @classmethod
+    def instance(cls, filepath):
+        if filepath not in cls.instances:
+            cls.instances[filepath] = cls(filepath)
+        return cls.instances[filepath]
+
+    def log(self, client_id, message):
+        self.clients.add(client_id)
+        if self.file is None:
+            try:
+                self.file = open(self.filepath, 'a')
+            except OSError:
+                # Nothing to be done.
+                return 
+        self.file.write(message)
+
+    def close(self, client_id):
+        if client_id in self.clients:
+            self.clients.remove(client_id)
+        if not self.clients:
+            # If all clients are done with the file, close it and clean up:
+            if self.file is not None:
+                self.file.flush()
+                self.file.close()
+            del self.instances[self.filepath]
+
+
 class ZMQLogServer(object):
-    def __init__(self, port=None, bind_address='tcp://0.0.0.0', silent=False):
+    def __init__(self, port=None, bind_address='tcp://127.0.0.1', silent=False):
         self.port = port
         self._initial_port = port
         self.bind_address = bind_address
         self.context = None
         self.router = None
         self.tasks = TaskQueue()
+        self.autoclose_tasks = {}
 
         self.run_thread = None
         self.stopping = False
         self.started = threading.Event()
         self.running = False
         self.silent = silent
+
+    def log(self, args):
+        # Validate arguments, but we can't reply if they are invalid. We just do nothing
+        # in that case.
+        if len(args) != 3:
+            return
+        client_id, filepath, message = args
+        try:
+            filepath = filepath.decode('utf8')
+            message = message.decode('utf8')
+        except UnicodeDecodeError:
+            return
+        logfile = LogFile.instance(filepath)
+        logfile.log(client_id, message)
+        self.cancel_autoclose(client_id)
+        self.set_autoclose(client_id, filepath)
+
+    def check_access(self, routing_id, args):
+        if len(args) != 1:
+            self.send(routing_id, ERR_WRONG_NUM_ARGS)
+            return
+        filepath = args[0]
+        try:
+            filepath = filepath.decode('utf8')
+        except UnicodeDecodeError:
+            self.send(routing_id, ERR_BAD_ENCODING)
+        try:
+            with open(filepath.decode('utf8'), 'a') as _:
+                pass
+        except OSError as e:
+            self.send(routing_id, b'error opening file: ' + str(e).encode('utf8'))
+
+    def close(self, routing_id, args):
+        if len(args) != 2:
+            self.send(routing_id, ERR_WRONG_NUM_ARGS)
+            return
+        client_id, filepath = args[0]
+        try:
+            filepath = filepath.decode('utf8')
+        except UnicodeDecodeError:
+            self.send(routing_id, ERR_BAD_ENCODING)
+            return
+        self.cancel_autoclose(client_id)
+        logfile = LogFile.instance(client_id)
+        logfile.close(client_id)
+
+    def set_autoclose(self, client_id, logfile):
+        """Add a task to close the file for the client after a timeout"""
+        task = Task(FILE_CLOSE_TIMEOUT, logfile.close, client_id)
+        self.tasks.add(task)
+        self.autoclose_tasks[client_id] = task
+
+    def cancel_autoclose(self, client_id):
+        """Cancel the scheduled auto-closing of the file for the client"""
+        task = self.autoclose_tasks.pop(client_id, None)
+        if task is not None:
+            self.tasks.cancel(self.autoclose_tasks[client_id])
 
     def run(self):
         self.context = zmq.Context.instance()
@@ -92,12 +185,18 @@ class ZMQLogServer(object):
                     # Not well formed as [routing_id, '', command, ...]
                     continue  # pragma: no cover
                 routing_id, command, args = request[0], request[2], request[3:]
-                if command == b'hello':
+                if command == b'log':
+                    self.log(args)
+                elif command == b'hello':
                     self.send(routing_id, b'hello')
                     logging.info("Someone said hello")
                 elif command == b'protocol':
                     self.send(routing_id, PROTOCOL_VERSION.encode('utf8'))
                     logging.info("Someone requested the protocol version")
+                elif command == b'check_access':
+                    self.check_access(routing_id, args)
+                elif command == b'close':
+                    self.close(routing_id, args)
                 elif command == b'stop' and self.stopping:
                     self.send(routing_id, b'ok')
                     break
@@ -136,6 +235,7 @@ class ZMQLogServer(object):
         self.stopping = False
 
     def send(self, routing_id, message):
+        print('sending:', message)
         self.router.send_multipart([routing_id, b'', message])
 
 
