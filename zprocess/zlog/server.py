fb4ec2ae7c9688b7b6a94e5fc833f9e84bdfa1ea
chrisjbillington <chrisjbillington@localhost>
2019-01-31 19:31:32 -0500

    Make zlock and zlog servers well-behaved with respect to routing, that is, have them treat everything preceding the empty message as routing information. This allows them to be used with proxies.

--- zprocess/zlog/server.py
+++ zprocess/zlog/server.py
@@ -290,10 +290,15 @@ class ZMQLogServer(object):
             if events:
                 # A request was received:
                 request = self.router.recv_multipart()
-                if len(request) < 3 or request[1] != b'':  # pragma: no cover
-                    # Not well formed as [routing_id, '', command, ...]
-                    continue  # pragma: no cover
-                routing_id, command, args = request[0], request[2], request[3:]
+                if b'' not in request:
+                    #  Malformed.
+                    continue
+                nroutes = request.index(b'')
+                routing_id, msg = request[:nroutes], request[nroutes+1:]
+                if not msg:  # pragma: no cover
+                    # Malformed
+                    continue
+                command, args = msg[0], msg[1:]
                 ip = self.router.peer_ip
                 if command == b'log':
                     self.log(args)
@@ -346,7 +351,7 @@ class ZMQLogServer(object):
         self.stopping = False
 
     def send(self, routing_id, message):
-        self.router.send_multipart([routing_id, b'', message])
+        self.router.send_multipart(routing_id + [b'', message])
 
 
 if __name__ == '__main__':
