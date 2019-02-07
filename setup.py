b5d184dd8d2f5a92f1f8ef3dbe3af829b007b791
Chris Billington <chrisjbillington@gmail.com>
2019-02-07 18:31:21 -0500

    Bump version for release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup
 import os
 
-VERSION = '2.11.3'
+VERSION = '2.11.4'
 
 # Auto generate a __version__ package for the package to import
 with open(os.path.join('zprocess', '__version__.py'), 'w') as f:
