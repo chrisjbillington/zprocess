bcd84b32565c487add10b986be55aac373120ad3
Chris Billington <chrisjbillington@gmail.com>
2019-05-02 21:50:06 -0400

    Bugfix: conflicting options in command line programs: -n conflicted with backup count of zlog server.

--- zprocess/zlog/__main__.py
+++ zprocess/zlog/__main__.py
@@ -188,7 +188,7 @@ def main():
     )
 
     exclusive_grp.add_argument(
-        '-n',
+        '-ni',
         '--no-allow-insecure',
         action='store_false',
         dest='allow_insecure',
